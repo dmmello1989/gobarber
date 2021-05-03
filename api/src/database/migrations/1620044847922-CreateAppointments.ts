6d574db40868a742d5c041e8c179a315da104991
Daniel Mello <dmmello1989@gmail.com>
2021-05-03 20:32:37 -0300

    integração com banco de dados ok

--- api/src/database/migrations/1620044847922-CreateAppointments.ts
+++ api/src/database/migrations/1620044847922-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1620044847922
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
